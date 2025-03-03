f1e612795c48366737377d473f8a41c91d4ddaf3
Echaib-Fatima <fatimaechaib22@gmail.com>
2025-03-03 10:41:44 +0000

    second commit

--- ex1.cjs
+++ ex1.cjs
@@ -1,1 +1,6 @@
-
+const readline = require ('readline-sync');
+let x = parseInt (readline.question("Entrer la valeur de x:"));
+let y = parseInt (readline.question("Entrer la valeur de y:"));
+[x,y]=[y,x];
+console.log("La valeur de x est:",x);
+console.log("La valeur de y est:",y);
